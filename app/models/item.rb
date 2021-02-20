e5a8b0170e91f367ac171e06631b9d72765943ea
kk-mandarin <74140194+kk-mandarin@users.noreply.github.com>
2021-02-20 22:41:54 +0900

    ActiveHash機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,26 @@
 class Item < ApplicationRecord
-belongs_to :user
-has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition_id
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+
+
+  belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :item, length: { maximum: 40 }
     validates :item_content, length: { maximum: 1000 }
-    validates :category_id
-    validates :condition_id
-    validates :delivery_fee_id
-    validates :prefecture_id
-    validates :shipping_date_id
+    with_options numericality: { other_than: 1 } do #id:1以外の時に保存できる
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :shipping_date_id
+    end
     validates :price
     validates :user
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "items#index"
-  #resources :usere, only [:index, :create] do
     resources :items, only: [:index, :new, :create]
-  #end
   #resources :items do
   #end
 end
