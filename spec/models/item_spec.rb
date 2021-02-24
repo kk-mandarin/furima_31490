19889482e9b72447fc4dbe9b2f14301d95376e8c
kk-mandarin <74140194+kk-mandarin@users.noreply.github.com>
2021-02-24 21:25:55 +0900

    商品出品機能実装_コード修正レビュー対応_take2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -95,7 +95,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not a number"
       end
-      it "prideが300未満では出品できない" do
+      it "priceが300未満では出品できない" do
         @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
@@ -104,6 +104,16 @@ RSpec.describe Item, type: :model do
         @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
+      end
+      it "priceが半角英数字混合では登録できないこと" do
+        @item.price = "aa11"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it "priceが半角英語だけでは登録できないこと" do
+        @item.price = "aaaab"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
       end
     end
   end
