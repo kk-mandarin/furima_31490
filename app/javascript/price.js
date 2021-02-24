1bb3e2b280065461e62388f5f3ee43fe55a9af43
kk-mandarin <74140194+kk-mandarin@users.noreply.github.com>
2021-02-24 21:35:20 +0900

    JavaScript_追加修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -2,11 +2,9 @@ window.addEventListener('load', function(){
 
   const addTaxDom = document.getElementById("add-tax-price");
   addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  console.log(addTaxDom);
+  
 
   const profitPrice = document.getElementById("profit");
     const taxValue = inputValue * 0.1;
-    console.log(taxValue);
     profitPrice.innerHTML = Math.floor(inputValue - taxValue);
-    console.log(profitPrice);
 })
