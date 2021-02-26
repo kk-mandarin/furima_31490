96a60b6c408635de83e67963f6385abb37a02363
kk-mandarin <74140194+kk-mandarin@users.noreply.github.com>
2021-02-26 22:05:40 +0900

    販売手数料自動計算_記述漏れ
    販売手数料の記述をで上記部分を削除してしまっていたようです。
    修正いたしましたので、いったんコミットさせていただきます。

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,10 +1,14 @@
 window.addEventListener('load', function(){
+  const priceInput = document.getElementById("price");
+priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+  
 
   const addTaxDom = document.getElementById("add-tax-price");
   addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  
 
   const profitPrice = document.getElementById("profit");
     const taxValue = inputValue * 0.1;
     profitPrice.innerHTML = Math.floor(inputValue - taxValue);
+  })
 })
