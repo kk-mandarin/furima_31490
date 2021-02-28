a4f962ac639f9d7b8049a965dc672a32009dd4b8
kk-mandarin <74140194+kk-mandarin@users.noreply.github.com>
2021-02-28 17:43:34 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :item_find, only: [:show, :edit, :update]
+  before_action :item_find, only: [:show, :edit, :update, :destroy ]
 
   def index
     @items = Item.order('created_at DESC')
@@ -33,6 +33,11 @@ class ItemsController < ApplicationController
       else
         render :edit
       end
+    end
+
+    def destroy
+      @item.destroy
+      redirect_to root_path
     end
 
   private
