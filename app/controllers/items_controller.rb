356464faa14610bfb9b3ec18ceb6ece98d353f79
kk-mandarin <74140194+kk-mandarin@users.noreply.github.com>
2021-02-28 12:50:58 +0900

    商品詳細編集_update機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, expect: [:index, :show]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_find, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -16,23 +18,34 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
 
     def show
-      @item = Item.find(params[:id])
     end
 
     def edit
-      @item = Item.find(params[:id])
+      redirect_to root_path unless current_user.id == @item.user_id
     end
 
-   
-
-  end
+    def update
+      if @item.update(item_params)
+        redirect_to item_path(@item)
+      else
+        render :edit
+      end
+    end
 
   private
 
-  def item_params
-    params.require(:item).permit(:image, :item, :item_content, :category_id, :condition_id, :delivery_fee_id, :prefecture_id,
-                                 :shipping_date_id, :price).merge(user_id: current_user.id)
-  end
+    def item_params
+      params.require(:item).permit(:image, :item, :item_content, :category_id, :condition_id, :delivery_fee_id, :prefecture_id,:shipping_date_id, :price).merge(user_id: current_user.id)
+    end
+
+    def move_to_index
+      redirect_to new_user_session_path unless user_signed_in?
+    end
+
+    def item_find
+      @item = Item.find(params[:id])
+    end
 end
